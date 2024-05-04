cc25f852a42e9a995f49c8d98daecb1fb4ae03d5
FerminBD <69362053+FerminBD@users.noreply.github.com>
2024-05-03 22:06:00 -0300

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialised`}> {children}
+      <footer className='py-10 flex justify-center items-center'>Hecho con Amor por la gente de Vercel</footer>
+      </body>
     </html>
   );
 }
